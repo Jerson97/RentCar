555d48c0fc0c3d31887e1a42d1b16f2cb0d0d461
Jerson97 <jersonramirezs97@gmail.com>
2025-04-28 10:48:54 -0500

    Feat: Se creo la capa Api y se agrego la dependecy injection

---
+++ src/RentCar.Api/Program.cs
@@ -0,0 +1,27 @@
+using RentCar.Application;
+using RentCar.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapControllers();
+
+
+app.Run();
